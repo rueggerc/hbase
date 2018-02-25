db1a5b529165aa82d82bf76a941d7f7929628898
Chris Ruegger <chris.ruegger@gmail.com>
2018-02-24 20:25:26 -0500

    Initial Checkin

---
+++ src/test/java/com/rueggerllc/hbase/tests/CensusTests.java
@@ -0,0 +1,42 @@
+package com.rueggerllc.hbase.tests;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CensusTests {
+
+	private static Logger logger = Logger.getLogger(CensusTests.class);
+	
+	
+	@BeforeClass
+	public static void setupClass() throws Exception {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setupTest() throws Exception {
+	}
+
+	@After
+	public void tearDownTest() throws Exception {
+	}
+	
+	@Test
+	@Ignore
+	public void dummyTest() {
+		logger.info("Dummy Test");
+	}
+	
+
+
+	
+	
+}
